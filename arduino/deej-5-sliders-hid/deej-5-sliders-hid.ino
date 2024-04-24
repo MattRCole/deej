a1e9ce020baa69c6ed08109372b68ef9fec2e121
MattRCole <matthewrichardcole@gmail.com>
2024-04-23 20:00:52 -0600

    Add some inital settings

---
+++ arduino/deej-5-sliders-hid/deej-5-sliders-hid.ino
@@ -0,0 +1,86 @@
+#include "deej-hid-definition.h"
+#include "hidgeneric.h"
+
+// CONSTANTS
+const int NUM_SLIDERS = 5;
+
+const int analogInputs[NUM_SLIDERS] = {9, 10, 11, 12, 13};
+int analogSliderValues[NUM_SLIDERS];
+int oldAnalogSliderValues[NUM_SLIDERS];
+bool sliderChange[NUM_SLIDERS];
+unsigned long lastPublishTime = 0;
+unsigned long lastClientCleanup = 0;
+
+// FNS
+
+void setup()
+{
+}
+
+bool shouldUpdateSliders()
+{
+  bool atLeastOneSliderChange = false;
+  for (int i = 0; i < NUM_SLIDERS; i++)
+  {
+    if (sliderChange[i])
+    {
+      atLeastOneSliderChange = true;
+      break;
+    }
+  }
+  return atLeastOneSliderChange;
+}
+
+void loop()
+{
+  updateSliderValues();
+  if (shouldUpdateSliders()) {
+    printSliderValues(); // For debug
+    sendSliderValues();
+  }
+  delay(10);
+}
+
+void updateSliderValues()
+{
+  for (int i = 0; i < NUM_SLIDERS; i++)
+  {
+    analogSliderValues[i] = (int)(analogRead(analogInputs[i]));
+    // analogSliderValues[i] = (int)(((u_int64_t)analogRead(analogInputs[i])) * 1023 / 4095);
+
+    // since we will be using wifi, we want to cut out jitter at the source.
+    // If your sliders have a travel of 60mm and range from 0 to 1023 in value
+    // this would be equivalent to ignoring any slider changes less than or equal to 0.18mm (7 thousandths of an inch)
+    const int totalChange = (int)oldAnalogSliderValues[i] - (int)analogSliderValues[i];
+
+    const bool significantSliderChange = totalChange <= -40 || totalChange >= 40;
+
+    if (significantSliderChange)
+    {
+      oldAnalogSliderValues[i] = analogSliderValues[i];
+      sliderChange[i] = true;
+    }
+  }
+}
+
+void sendSliderValues()
+{
+}
+
+void printSliderValues()
+{
+  for (int i = 0; i < NUM_SLIDERS; i++)
+  {
+    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV, ") + String("Changed: ") + String(sliderChange[i]);
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_SLIDERS - 1)
+    {
+      Serial.write(" | ");
+    }
+    else
+    {
+      Serial.write("\n");
+    }
+  }
+}
